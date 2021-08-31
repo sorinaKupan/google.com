8f10d52a903316c3d1775573d507776655454721
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-08-31 14:54:13 +0300

    Created a new project. Added index.html - the file where the page will be developed.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace google.com
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

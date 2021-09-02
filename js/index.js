2a6ce5ea443c1e164be204db564880ae23c09339
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-09-02 09:59:42 +0300

    Builded user menu onclick. Implemented css and js for the menu.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 function redirectToPage(event) {
     if(event.keyCode == 13){
-        var word = document.querySelector("#query");
-        window.location.href = "https://www.google.com/search?q="+ word.value;
+        var word = document.querySelector("query");
+        window.location.href = "https://www.google.com/search?q="+ word.textContent;
+    }
+}
+function visibleMenu(){
+    if (document.getElementById("userMenu").style.visibility = "hidden"){
+       document.getElementById("userMenu").style.visibility = "visible"
+    }
+    else if(document.getElementById("userMenu").style.visibility = "visible"){
+        document.getElementById("userMenu").style.visibility = "hidden"
     }
 }
